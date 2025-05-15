49033507cee1665dbba07551e99b7b048ebc1ef2
Betsabe Jr Hoyos Barrios 👨‍💻 <184250960+programador-fuentes-especificas-opa@users.noreply.github.com>
2025-05-15 12:06:28 -0500

    feat(IdentificationTypeRepository): Se crea repositorio oara los tipos de identificacion

---
+++ UserManager.Infrastructure/Repositories/IdentificationTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="IdentificationTypeRepository.cs" company="DITOS SAS">
+// Copyright (c) DITOS SAS. All rights reserved.
+// </copyright>
+
+namespace UserManager.Infrastructure.Repositories
+{
+    using Microsoft.EntityFrameworkCore;
+    using UserManager.Domain.Entities;
+    using UserManager.Infrastructure.Data;
+    using UserManager.Infrastructure.Interfaces;
+
+    public class IdentificationTypeRepository : IIdentificationTypeRepository
+    {
+        private readonly UserManagerDbContext _context;
+
+        public IdentificationTypeRepository(UserManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<IdentificationType>> GetAllAsync()
+        {
+            return await _context.Identities.ToListAsync();
+        }
+    }
+}
