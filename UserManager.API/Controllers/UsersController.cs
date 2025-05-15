aeac81752834dac99d18419928b62cb5fa15d22c
Betsabe Jr Hoyos Barrios 👨‍💻 <programador-fuentes-especificas@opa.com.co>
2025-05-15 07:55:01 -0500

    Refactor UsersController y mejora la documentación
    Se eliminó la importación innecesaria de `UserManager.Application.Services` y se añadieron comentarios descriptivos en el código.
    El método `GetById` ahora devuelve un `User?`, permitiendo retornar `null` para IDs no válidos.
    Se implementaron los métodos `Update` y `Delete` con validaciones adecuadas y respuestas de error.
    Además, se mejoró la documentación XML para una mejor comprensión de la funcionalidad de cada método.

--- UserManager.API/Controllers/UsersController.cs
+++ UserManager.API/Controllers/UsersController.cs
@@ -7,35 +7,60 @@ namespace UserManager.API.Controllers
     using Microsoft.AspNetCore.Mvc;
     using UserManager.Application.DTOS;
     using UserManager.Application.Interfaces;
-    using UserManager.Application.Services;
     using UserManager.Domain.Entities;
 
     /// <summary>
-    /// This is controller the user.
+    /// This class is the user controller.
+    /// It helps to create, read, update and delete users.
     /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
+        // This is the service to manage users.
         private readonly IUserService _userService;
 
+        /// <summary>
+        /// This is the constructor.
+        /// It receives the user service.
+        /// </summary>
+        /// <param name="userService">This is the user service.</param>
         public UsersController(IUserService userService)
         {
             _userService = userService;
         }
 
+        /// <summary>
+        /// This method gets all users.
+        /// </summary>
+        /// <returns>List of users.</returns>
         [HttpGet]
         public async Task<IEnumerable<User>> GetAll()
         {
             return await _userService.GetAllAsync();
         }
 
+        /// <summary>
+        /// This method gets one user by ID.
+        /// </summary>
+        /// <param name="id">User ID.</param>
+        /// <returns>The user with the given ID.</returns>
         [HttpGet("{id}")]
-        public async Task<User> GetById(int id)
+        public async Task<User?> GetById(int id)
         {
+            if (id <= 0)
+            {
+                return null;
+            }
+
             return await _userService.GetByIdAsync(id);
         }
 
+        /// <summary>
+        /// This method creates a new user.
+        /// </summary>
+        /// <param name="dto">This is the new user data.</param>
+        /// <returns>A response with the new user.</returns>
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateUserRequest dto)
         {
@@ -52,6 +77,59 @@ namespace UserManager.API.Controllers
             var createdUser = await _userService.AddAsync(user);
 
             return CreatedAtAction(nameof(GetAll), new { id = createdUser.Id }, createdUser);
+        }
+
+        /// <summary>
+        /// This method updates an existing user.
+        /// </summary>
+        /// <param name="id">The ID of the user to update.</param>
+        /// <param name="dto">The new data for the user.</param>
+        /// <returns>A response with status OK or error.</returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateUserRequest dto)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userService.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.FirstName = dto.FirstName;
+            user.LastName = dto.LastName;
+            user.IdentificationTypeId = dto.IdentificationTypeId;
+            user.IdentificationNumber = dto.IdentificationNumber;
+            user.Email = dto.Email;
+            user.Password = dto.Password;
+            await _userService.UpdateAsync(user);
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// This method deletes a user.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
+        /// <returns>A response with status OK or error.</returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userService.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userService.DeleteAsync(id);
+            return Ok();
         }
     }
 }
