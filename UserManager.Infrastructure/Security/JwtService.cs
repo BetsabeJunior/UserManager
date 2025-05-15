73b3b0c0b8463e98a5bcadcf8b4c7eba6f693455
Betsabe Jr Hoyos Barrios 👨‍💻 <184250960+programador-fuentes-especificas-opa@users.noreply.github.com>
2025-05-15 15:02:59 -0500

    style(JwtService): Se coloca StyleCop

--- UserManager.Infrastructure/Security/JwtService.cs
+++ UserManager.Infrastructure/Security/JwtService.cs
@@ -1,26 +1,42 @@
-﻿// <copyright file="JwtService.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="JwtService.cs" company="DITO SAS">
+// Copyright (c) DITO SAS. All rights reserved.
 // </copyright>
 
 namespace UserManager.Infrastructure.Security
 {
     using Microsoft.Extensions.Configuration;
     using Microsoft.IdentityModel.Tokens;
+    using System;
     using System.IdentityModel.Tokens.Jwt;
     using System.Security.Claims;
     using System.Text;
     using UserManager.Domain.Entities;
     using UserManager.Infrastructure.Interfaces;
 
+    /// <summary>
+    /// This class help make token (JWT) for user login.
+    /// </summary>
     public class JwtService : IJwtService
     {
+        /// <summary>
+        /// This is config. It has secret key and info for JWT.
+        /// </summary>
         private readonly IConfiguration _configuration;
 
+        /// <summary>
+        /// This is constructor. It puts config inside the class.
+        /// </summary>
+        /// <param name="configuration">This is the config from app settings.</param>
         public JwtService(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
+        /// <summary>
+        /// This method make a JWT token for one user.
+        /// </summary>
+        /// <param name="user">This is the user to make token.</param>
+        /// <returns>This give back a token in string.</returns>
         public string GenerateToken(User user)
         {
             var claims = new[]
@@ -31,6 +47,7 @@ namespace UserManager.Infrastructure.Security
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
@@ -38,8 +55,7 @@ namespace UserManager.Infrastructure.Security
                 audience: _configuration["Jwt:Audience"],
                 claims: claims,
                 expires: DateTime.UtcNow.AddMinutes(30),
-                signingCredentials: credentials
-            );
+                signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
