a906b6380ea2f39da5da7b1d62dd5d62d398b7d1
Betsabe Jr Hoyos Barrios 👨‍💻 <184250960+programador-fuentes-especificas-opa@users.noreply.github.com>
2025-05-15 11:59:05 -0500

    feat(AuthenticationLoginController): Se crea controlador para los tipos de identificacion

---
+++ UserManager.API/Controllers/AuthenticationLoginController.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="AuthenticationLoginController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace UserManager.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using UserManager.Application.DTOS;
+    using UserManager.Application.Interfaces;
+    using UserManager.Domain.Entities;
+
+    /// <summary>
+    /// This class is the Authentication Login controller.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationLoginController : Controller
+    {
+        // This is the service to manage users.
+        private readonly IUserService _userService;
+
+        public AuthenticationLoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// This method gets the token, fullName and email the users.
+        /// </summary>
+        /// <returns>List of users.</returns>
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var response = await _userService.AuthenticateAsync(request);
+
+            if (response == null)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ UserManager.API/Controllers/IdentificationTypesController.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="IdentificationTypesController.cs" company="DITOS SAS">
+// Copyright (c) DITOS SAS. All rights reserved.
+// </copyright>
+
+namespace UserManager.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using UserManager.API.Common.Responses;
+    using UserManager.Infrastructure.Interfaces;
+    using Microsoft.AspNetCore.Authorization;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdentificationTypesController : ControllerBase
+    {
+        private readonly IIdentificationTypeRepository _repository;
+        private readonly ILogger<IdentificationTypesController> _logger;
+
+        public IdentificationTypesController(IIdentificationTypeRepository repository, ILogger<IdentificationTypesController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [Authorize]
+        [HttpGet("GetAllIdentificationType")]
+        public async Task<IActionResult> GetAll()
+        {
+            _logger.LogInformation("Getting all type identifications");
+            var types = await _repository.GetAllAsync();
+            return Ok(types);
+        }
+    }
+}
