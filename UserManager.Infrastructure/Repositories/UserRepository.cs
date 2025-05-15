466f67e20b606ba89e6d8508393d0d5001bca03f
Betsabe Jr Hoyos Barrios 👨‍💻 <programador-fuentes-especificas@opa.com.co>
2025-05-15 07:57:37 -0500

    fix(UserRepository): Se agrega comentarios de stylecop

--- UserManager.Infrastructure/Repositories/UserRepository.cs
+++ UserManager.Infrastructure/Repositories/UserRepository.cs
@@ -4,7 +4,6 @@
 
 namespace UserManager.Infrastructure.Repositories
 {
-
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
@@ -13,22 +12,26 @@ namespace UserManager.Infrastructure.Repositories
     using UserManager.Infrastructure.Data;
 
     /// <summary>
-    /// Class to work with users in database.
+    /// This class helps to work with users in the database.
+    /// You can get, add, update, or delete users.
     /// </summary>
     public class UserRepository : IUserRepository
     {
         private readonly UserManagerDbContext _context;
 
         /// <summary>
-        /// Constructor with database context.
+        /// Creates the repository with the database connection.
         /// </summary>
-        /// <param name="context">Database context.</param>
+        /// <param name="context">The database connection.</param>
         public UserRepository(UserManagerDbContext context)
         {
             _context = context;
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Get all users from the database.
+        /// </summary>
+        /// <returns>List of all users.</returns>
         public async Task<IEnumerable<User>> GetAllAsync()
         {
             return await _context.Users
@@ -36,13 +39,23 @@ namespace UserManager.Infrastructure.Repositories
                 .ToListAsync();
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Get one user by their ID.
+        /// </summary>
+        /// <param name="id">The user's ID.</param>
+        /// <returns>The user with that ID or null.</returns>
         public async Task<User> GetByIdAsync(int id)
         {
-            return await _context.Users.FindAsync(id);
+            return await _context.Users
+                .Include(u => u.IdentificationType)
+                .FirstOrDefaultAsync(u => u.Id == id);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Add a new user to the database.
+        /// </summary>
+        /// <param name="user">The user to add.</param>
+        /// <returns>The user that was added.</returns>
         public async Task<User> AddAsync(User user)
         {
             _context.Users.Add(user);
@@ -50,14 +63,20 @@ namespace UserManager.Infrastructure.Repositories
             return user;
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Update an existing user in the database.
+        /// </summary>
+        /// <param name="user">The user with updated information.</param>
         public async Task UpdateAsync(User user)
         {
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Delete a user by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
         public async Task DeleteAsync(int id)
         {
             var user = await _context.Users.FindAsync(id);
