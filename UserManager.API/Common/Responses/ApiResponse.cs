e48b4f101cd809375a2372b3e37b3ebcfebb1c5f
Betsabe Jr Hoyos Barrios 👨‍💻 <184250960+programador-fuentes-especificas-opa@users.noreply.github.com>
2025-05-15 11:58:34 -0500

    feat(ApiResponse): Se crea respuesta de la API

---
+++ UserManager.API/Common/Responses/ApiResponse.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="ApiResponse.cs" company="PlaceholderCompany">
+// Copyright (c) DITOS SAS. All rights reserved.
+// </copyright>
+
+namespace UserManager.API.Common.Responses
+{
+    /// <summary>
+    /// Standard response format for API.
+    /// </summary>
+    /// <typeparam name="T">Type of data to return.</typeparam>
+    public class ApiResponse<T>
+    {
+        /// <summary>
+        /// Indicates if the response is success or error.
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Message about the response.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Data returned (can be null on error).
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Create a success response.
+        /// </summary>
+        /// <param name="data">Data to send.</param>
+        /// <param name="message">Optional message.</param>
+        /// <returns>ApiResponse with success true.</returns>
+        public static ApiResponse<T> Ok(T data, string message = "")
+        {
+            return new ApiResponse<T> { Success = true, Message = message, Data = data };
+        }
+
+        /// <summary>
+        /// Create a fail response.
+        /// </summary>
+        /// <param name="message">Error message.</param>
+        /// <returns>ApiResponse with success false.</returns>
+        public static ApiResponse<T> Fail(string message)
+        {
+            return new ApiResponse<T> { Success = false, Message = message, Data = default };
+        }
+    }
+}
