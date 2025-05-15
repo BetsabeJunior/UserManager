830dac248bc805f1fe373f86fe72e91afe48e0c5
Betsabe Jr Hoyos Barrios 👨‍💻 <184250960+programador-fuentes-especificas-opa@users.noreply.github.com>
2025-05-15 15:02:11 -0500

    fix(Infrastructure): Se coloca StyleCop

--- UserManager.Infrastructure/Interfaces/IIdentificationTypeRepository.cs
+++ UserManager.Infrastructure/Interfaces/IIdentificationTypeRepository.cs
@@ -1,13 +1,28 @@
-﻿// <copyright file="IIdentificationTypeRepository.cs" company="DITOS SAS">
-// Copyright (c) DITOS SAS. All rights reserved.
+﻿// <copyright file="IIdentificationTypeRepository.cs" company="DITO SAS">
+// Copyright (c) DITO SAS. All rights reserved.
 // </copyright>
 
 namespace UserManager.Infrastructure.Interfaces
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using UserManager.Domain.Entities;
 
+    /// <summary>
+    /// Provides access to identification types from the data source.
+    /// </summary>
     public interface IIdentificationTypeRepository
     {
+        /// <summary>
+        /// Gets all identification types asynchronously.
+        /// </summary>
+        /// <returns>A list of identification types.</returns>
         Task<List<IdentificationType>> GetAllAsync();
+
+        /// <summary>
+        /// Gets identification by id.
+        /// </summary>
+        /// <returns>A identification types.</returns>
+        Task<IdentificationType> GetByIdAsync(int id);
     }
 }
--- UserManager.Infrastructure/Interfaces/IJwtService.cs
+++ UserManager.Infrastructure/Interfaces/IJwtService.cs
@@ -1,13 +1,21 @@
-﻿// <copyright file="IJwtService.cs" company="Dito SAS">
-// Copyright (c) Dito SAS. All rights reserved.
+﻿// <copyright file="IJwtService.cs" company="DITO SAS">
+// Copyright (c) DITO SAS. All rights reserved.
 // </copyright>
 
 namespace UserManager.Infrastructure.Interfaces
 {
     using UserManager.Domain.Entities;
 
+    /// <summary>
+    /// Provides functionality to generate JSON Web Tokens (JWT) for users.
+    /// </summary>
     public interface IJwtService
     {
+        /// <summary>
+        /// Generates a JWT token for the specified user.
+        /// </summary>
+        /// <param name="user">The user for whom to generate the token.</param>
+        /// <returns>A JWT token as a string.</returns>
         string GenerateToken(User user);
     }
 }
